9965f419a4fcb4e069d94c0fdf021e7a7568b1d7
Danny Stoll <stolld@cs.uni-freiburg.de>
2021-08-05 20:06:01 +0200

    provide new service loop

--- metahyper/_networking.py
+++ metahyper/_networking.py
@@ -1,10 +1,11 @@
 import atexit
 import fcntl
+import logging
 import multiprocessing
 import socket
 import socketserver
+import time
 from pathlib import Path
-from time import sleep
 
 import dill
 import netifaces
@@ -16,7 +17,8 @@ def nic_name_to_host(nic_name):
         # See https://pypi.org/project/netifaces/
         host = netifaces.ifaddresses(nic_name).setdefault(
             netifaces.AF_INET, [{"addr": "No IP addr"}]
-        )[0]["addr"]
+        )
+        host = host[0]["addr"]
     except ValueError:
         raise ValueError(
             f"You must specify a valid interface name. "
@@ -25,69 +27,7 @@ def nic_name_to_host(nic_name):
     return host
 
 
-class Sampler:
-    def __init__(self, config_space):
-        self.config_space = config_space
-        self.results = []
-
-    def new_result(self, result):
-        self.results.append(result)
-
-    def get_config(self):
-        return len(self.results)
-
-
-class MasterServerHandler(socketserver.BaseRequestHandler):
-    """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    sampler = None
-
-    def handle(self):
-        self.data = data = self.request.recv(1024).strip()
-        self.sampler.new_result(data)
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        config = self.sampler.get_config()
-        self.request.sendall(dill.dumps(config))
-
-
-def start_master_server():
-    HOST, PORT = "localhost", 9999
-    MasterServerHandler.sampler = Sampler(dict())  # TODO: explain necessity for the dirty
-    socketserver.TCPServer.allow_reuse_address = True  # Do we really want this?
-    with socketserver.TCPServer((HOST, PORT), MasterServerHandler) as server:
-        server.timeout = 10
-        try:
-            while True:
-                server.handle_request()
-        finally:
-            server.shutdown()
-
-
-def start_worker_client():
-    HOST, PORT = "localhost", 9999
-    data = "a"
-
-    # Create a socket (SOCK_STREAM means a TCP socket)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        # Connect to server and send data
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes(data + "\n", "utf-8"))
-
-        # Receive data from the server and shut down
-        received = dill.loads(sock.recv(1024))
-
-    print("Sent:     {}".format(data))
-    print("Received: {}".format(received))
-
-
-class MasterLocker:
+class _MasterLocker:
     def __init__(self, lock_path):
         atexit.register(self.__del__)
         master_lock_file = Path(lock_path)
@@ -105,24 +45,130 @@ class MasterLocker:
             return False
 
 
-def start_loop():
-    master_locker = MasterLocker(".lock_master")
+class _MasterServerHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    sampler = None
+
+    def handle(self):
+        data = dill.loads(self.request.recv(1024).strip())
+        self.sampler.new_result(data)
+        print("{} wrote:".format(self.client_address[0]))
+        print(data)
+        config = self.sampler.get_config()
+        self.request.sendall(dill.dumps(config))
+
+
+def _make_request(host, port, data, receive_something=False):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.connect((host, port))
+        except ConnectionRefusedError:
+            print("something bad happened")  # TODO: handle
+        sock.sendall(dill.dumps(data))
+        print("Sent:     {}".format(data))
+
+        if receive_something:
+            received = dill.loads(sock.recv(1024))  # TODO: error handling
+            print("Received: {}".format(received))
+            return received
+
+
+def _start_master_server(host, port, sampler, timeout=10):
+    _MasterServerHandler.sampler = sampler  # TODO: explain necessity for the dirty
+    # https://stackoverflow.com/questions/22549044/why-is-port-not-immediately-released-after-the-socket-closes
+    socketserver.TCPServer.allow_reuse_address = True  # Do we really want this?
+    with socketserver.TCPServer((host, port), _MasterServerHandler) as server:
+        server.timeout = timeout
+        try:
+            while True:
+                server.handle_request()
+        finally:
+            server.shutdown()
+
+
+def service_loop(host, port, evaluation_fn, sampler, master_handling_timeout=10):
+    # TODO: add host port scan
+
+    # TODO proper logging handling
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    master_locker = _MasterLocker(".lock_master")
     master_process = None
+    evaluation_process = None
     while True:
-        if master_process is None and master_locker.acquire_lock():
-            sleep(2)
+        # Master activities
+        if master_process is not None and not master_process.is_alive():
+            logger.info("TODO release lock?")
+        elif master_process is None and master_locker.acquire_lock():
+            time.sleep(2)
+            logger.info("Starting master server")
             master_process = multiprocessing.Process(
-                name="master_server", target=start_master_server, daemon=True
+                name="master_server",
+                target=_start_master_server,
+                kwargs=dict(
+                    host=host, port=port, sampler=sampler, timeout=master_handling_timeout
+                ),
+                daemon=True,
             )
             master_process.start()
-            print("started master")
-        elif master_process is not None and not master_process.is_alive():
-            print("TODO release lock?")
-        else:
-            start_worker_client()
-            print("started worker")
-        sleep(10)
+
+        time.sleep(2)
+
+        # Worker activities
+        if evaluation_process is None or not evaluation_process.is_alive():
+            # TODO: read out result
+            # TODO: Serialize result to disk
+
+            # TODO: request symbols
+            # TODO: read out master location from disk
+            # TODO: error handling in case master is dead
+            logger.info("Requesting new config")
+            new_config = _make_request(
+                host, port, "give_me_new_config", receive_something=True
+            )
+
+            logger.info(f"Starting up new evaluation process with config {new_config}")
+            evaluation_process = multiprocessing.Process(
+                name="evaluation_process",
+                target=evaluation_fn,
+                kwargs=dict(
+                    config=new_config,
+                    config_working_directory=".",
+                    previous_working_directory=None,
+                ),
+                daemon=True,
+            )
+            evaluation_process.start()
+        elif True:  # TODO condition
+            logger.info("Letting master know I am still alive")
+            _make_request(host, port, "am_alive", receive_something=False)
+
+        time.sleep(5)
 
 
 if __name__ == "__main__":
-    start_loop()
+
+    class Sampler:
+        def __init__(self, config_space):
+            self.config_space = config_space
+            self.results = []
+
+        def new_result(self, result):
+            self.results.append(result)
+
+        def get_config(self):
+            return len(self.results)
+
+    def evaluation_fn(config, config_working_directory, previous_working_directory):
+        time.sleep(20)
+        return "evald"
+
+    service_loop("localhost", 9999, evaluation_fn, Sampler(dict()))
